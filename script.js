d9d4d6d33c4c60296a07b2e4e817fdb944706827
Astra <youloveda@mail.ru>
2026-07-09 13:24:23 +0300

    демо 0.5

--- script.js
+++ script.js
@@ -2,8 +2,7 @@
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('open');
-    
-    
+       
     const icon = this.querySelector('i');
     if (navLinks.classList.contains('open')) {
         icon.className = 'fas fa-times';
