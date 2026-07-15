3c626a7ebc3661b7709f42ed83ac6fcf968727b6
Astra <youloveda@mail.ru>
2026-07-15 20:20:10 +0300

    редакция главной страницы

---
+++ js/script_smooth.js
@@ -0,0 +1,7 @@
+function smoothScroll(event, targetId) {
+    event.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    targetElement.scrollIntoView({
+        behavior: 'smooth'
+    });
+}
